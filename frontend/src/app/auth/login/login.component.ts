387d87d668331a57d5f414315d38586c7526209b
Fabi <jessipandita2616@gmail.com>
2023-05-24 15:52:30 -0600

    Se agrega formulario ts y válidaciones login

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public formLogin! : FormGroup;
+  public hide : boolean = true;
+  
+  constructor(
+    private fb : FormBuilder,
+  ){ 
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.fb.group({
+      correo : ['',[Validators.email,Validators.required]],
+      password: ['',[Validators.required]]
+    })
+  }
+
+  getErrorMessage() {
+    if (this.formLogin.get('correo')?.hasError('required')) {
+      return 'Campo obligatorio';
+    }
+
+    return this.formLogin.get('correo')?.hasError('email') ? 'Correo inválido' : '';
+  }
 }
