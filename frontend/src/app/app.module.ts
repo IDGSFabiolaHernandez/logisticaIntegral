cf2c89eb43cb9135215a554fe034a34698a5969d
Adrián Villa <al221911456@gmail.com>
2023-05-25 09:16:15 -0600

    Reestructura bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routing';
@@ -18,7 +17,6 @@ import { AppRoutes } from './app.routing';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
-    MaterialModule,
     RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
