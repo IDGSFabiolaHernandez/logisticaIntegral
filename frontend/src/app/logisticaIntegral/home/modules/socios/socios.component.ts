84833f7d3d306bcd1eec58b551308108be621c73
Adrián Villa <al221911456@gmail.com>
2023-05-29 09:58:52 -0600

    Correción headers de tablas genericas e idioma

--- frontend/src/app/logisticaIntegral/home/modules/socios/socios.component.ts
+++ frontend/src/app/logisticaIntegral/home/modules/socios/socios.component.ts
@@ -14,6 +14,9 @@ export class SociosComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.dtOptions = {
+      language: {
+        url : "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+      },
       columns: [
         {
           title : '#',
