55f99b24e5ed41ca8b3bb9956083957c77a71bca
Fabi <jessipandita2616@gmail.com>
2023-05-29 17:50:35 -0600

    Se agrega servicio para consulta general de socios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', 'App\Http\Controllers\Auth\LoginController@login');
 Route::post('socios/listaSocios', 'App\Http\Controllers\LogisticaIntegral\SociosController@obtenerListaSocios');
+Route::get('socios/sociosGenerales', 'App\Http\Controllers\LogisticaIntegral\SociosController@obtenerSociosGenerales');
