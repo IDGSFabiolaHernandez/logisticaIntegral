f314eb06e4df4b553e9195d89b625450cd40a301
Adrián Villa <al221911456@gmail.com>
2023-05-30 17:01:28 -0600

    Se agrega funcionalidad filtros individuales para grid generico

--- frontend/src/app/logisticaIntegral/home/modules/socios/socios.component.ts
+++ frontend/src/app/logisticaIntegral/home/modules/socios/socios.component.ts
@@ -32,6 +32,16 @@ export class SociosComponent implements OnInit {
 	sortBy: string = '';
   	sortDesc: boolean = false;
 
+	filterValues : any = {
+		id: '',
+		nombreSocio: '',
+		status: '',
+		numEmpresas: '',
+		curpSocio: '',
+		rfcSocio: '',
+		nombreIntermediario: ''
+	};	  
+
 	constructor (
 		private mensajes : MensajesService,
 		private apiSocios : SociosService
@@ -60,8 +70,15 @@ export class SociosComponent implements OnInit {
 	get paginatedItems() {
 		const startIndex = (this.currentPage - 1) * this.itemsPerPage;
 		const endIndex = startIndex + this.itemsPerPage;
-		return this.listaSocios.slice(startIndex, endIndex);
+	  
+		return this.listaSocios.filter((socio) => {
+			return Object.keys(this.filterValues).every((column) => {
+				const filter = this.filterValues[column].toLowerCase();
+				return socio[column].toString().toLowerCase().includes(filter);
+			});
+		}).slice(startIndex, endIndex);
 	}
+	  
 	
 	get totalPages() {
 		return Math.ceil(this.listaSocios.length / this.itemsPerPage);
@@ -95,23 +112,23 @@ export class SociosComponent implements OnInit {
 
 	sortColumn(column: string) {
 		if (this.sortBy === column) {
-		  this.sortDesc = !this.sortDesc;
+		  	this.sortDesc = !this.sortDesc;
 		} else {
-		  this.sortBy = column;
-		  this.sortDesc = false;
+			this.sortBy = column;
+			this.sortDesc = false;
 		}
 	
 		this.listaSocios.sort((a, b) => {
-		  const valueA = a[column];
-		  const valueB = b[column];
-	
-		  if (valueA < valueB) {
-			return this.sortDesc ? 1 : -1;
-		  } else if (valueA > valueB) {
-			return this.sortDesc ? -1 : 1;
-		  } else {
-			return 0;
-		  }
+			const valueA = a[column];
+			const valueB = b[column];
+		
+			if (valueA < valueB) {
+				return this.sortDesc ? 1 : -1;
+			} else if (valueA > valueB) {
+				return this.sortDesc ? -1 : 1;
+			} else {
+				return 0;
+			}
 		});
 	}
 
