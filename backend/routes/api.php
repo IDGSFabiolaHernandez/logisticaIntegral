a5195fd24184a66a6c38b01cb12409516380b897
Fabi <jessipandita2616@gmail.com>
2023-05-30 10:00:57 -0600

    Se separa servicio de socios con socios select

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/login', 'App\Http\Controllers\Auth\LoginController@login');
 Route::post('socios/listaSocios', 'App\Http\Controllers\LogisticaIntegral\SociosController@obtenerListaSocios');
 Route::get('socios/sociosGenerales', 'App\Http\Controllers\LogisticaIntegral\SociosController@obtenerSociosGenerales');
+Route::get('socios/obtenerSociosSelect', 'App\Http\Controllers\LogisticaIntegral\SociosController@obtenerOpcionesSocios');
